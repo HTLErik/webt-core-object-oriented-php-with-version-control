1b07ea02a65fa21690816a671204b54863bacd0b
HTLErik <9054@htl.rennweg.at>
2022-10-16 19:09:05 +0200

    User Story 7

---
+++ VideoYouTube.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'Video.php';
+
+class VideoYouTube extends Video
+{
+
+    public function __construct($name, $source)
+    {
+        $videoId = explode('=', $source);
+        $videoId = end($videoId);
+        $embedHtml = '<section class="flex flex-row w-full justify-evenly">
+        <iframe width="700" height="400" src="https://www.youtube.com/embed/'.$videoId.'"
+            title="'.$name.'" frameborder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+    </section>
+    <br>
+    <p class="text-center text-xl font-bold">'.$name.'</p>
+    <br>';
+        parent::__construct($name, $source, $embedHtml);
+        
+    }
+}
+?>
